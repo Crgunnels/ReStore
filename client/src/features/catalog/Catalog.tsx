b58c1f86d2d6233d7df86c1bfde1b76a98ba3545
Crgunnels <92119195+Crgunnels@users.noreply.github.com>
2022-04-03 14:56:02 -0400

    Stopping point 3

---
+++ client/src/features/catalog/Catalog.tsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import ProductList from "./ProductList";
+import { Product } from "../../app/models/product"
+
+
+export default function Catalog(){
+  const [products, setProducts] = useState<Product[]>([]);
+    
+      useEffect(() => {
+        fetch('http://localhost:5000/api/products')
+        .then(response => response.json())
+        .then(data => setProducts(data))
+      }, [])
+    return(
+        <>
+        <ProductList products={products}/>    
+      </>
+    )
+}
+
+
+
